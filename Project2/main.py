443ceda8ccc29b88b469bd618170389fac61bd4c
GariiiZyu <92561657+galinajialinzhu@users.noreply.github.com>
2023-10-20 11:03:27 -0700

    Update main.py

--- Project2/main.py
+++ Project2/main.py
@@ -16,7 +16,18 @@ def draw():
   circle_size = int(data)
   p5.noStroke()
   p5.fill(150)
-  p5.ellipse(150, 150, circle_size, circle_size)
+  #p5.ellipse(150, 150, circle_size, circle_size)
+  p5.push()
+  angle = int(data)
+  #move it to middle of canvas
+  p5.translate(p5.width/2, p5.height/2)
+  #change it from degrees to radians
+  p5.ritate(p5.radians(angle))
+  #draw it from center
+  p5.rectMode(p5.CENTER)
+  #draw rec at this point
+  p5.rect(0,0,100,100)
+  p5.pop()
 
 def print_test(x):
   print(x)
